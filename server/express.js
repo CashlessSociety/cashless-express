953ef9f4c79919a97629cb10d23a345d121ec202
Miles Thompson <utunga@gmail.com>
2020-09-29 22:12:24 +1300

    Improved (i hope) the error tracking and logging
    Was finding it a bit frustating the way errors weren't showing up. Did a bit of research pretty sure this is the way.

--- server/express.js
+++ server/express.js
@@ -1,48 +1,53 @@
 /* eslint consistent-return:0 import/order:0 */
 
 const express = require('express');
+require('express-async-errors');
 const logger = require('./logger');
 const argv = require('./argv');
 const port = require('./port');
 const setup = require('./middlewares/frontendMiddleware');
 const isDev = process.env.NODE_ENV !== 'production';
-const ssb = require("./lib/ssb-client");
+const ssb = require('./lib/ssb-client');
 const https = require('https');
 const app = express();
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const fileUpload = require("express-fileupload");
-const cookieEncrypter = require("cookie-encrypter");
-const axios = require("axios");
-const fs = require("fs");
-const { sentry } = require("./lib/errors");
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+const cookieEncrypter = require('cookie-encrypter');
+const axios = require('axios');
+const fs = require('fs');
+const { sentry } = require('./lib/errors');
 const cors = require('cors');
-const exec = require('child_process').exec;
+const { exec } = require('child_process');
+// raise errors like this - throw httpErrorInfo(status, message, properties)
+const httpErrorInfo = require('http-errors');
 
-const ngrok = (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel ? require('ngrok') : false;
+const ngrok =
+  (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel
+    ? require('ngrok')
+    : false;
 const { resolve } = require('path');
 
 const cookieOptions = {
-    httpOnly: true,
-    signed: true,
-    expires: new Date(253402300000000), // Friday, 31 Dec 9999 23:59:59 GMT, nice date from stackoverflow
-    sameSite: "Lax",
+  httpOnly: true,
+  signed: true,
+  expires: new Date(253402300000000), // Friday, 31 Dec 9999 23:59:59 GMT, nice date from stackoverflow
+  sameSite: 'Lax',
 };
 
-const keyshareDir = process.env.KEYSHARE_DIR || __dirname+'/';
+const keyshareDir = process.env.KEYSHARE_DIR || `${__dirname}/`;
 console.log('key and key commands dir:', keyshareDir);
 
-const sh = cmd => {
-    return new Promise(function (resolve, reject) {
-      exec(cmd, (err, stdout, stderr) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve({ stdout, stderr });
-        }
-      });
+const sh = cmd =>
+  new Promise(function(resolve, reject) {
+    exec(cmd, (err, stdout, stderr) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve({ stdout, stderr });
+      }
     });
-}
+  });
 
 if (sentry) {
   // Sentry request handler must be the first middleware on the app
@@ -53,7 +58,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
 
 const cookieSecret =
-  process.env.COOKIES_SECRET || "set_cookie_secret_you_are_unsafe"; // has to be 32-bits
+  process.env.COOKIES_SECRET || 'set_cookie_secret_you_are_unsafe'; // has to be 32-bits
 
 app.use(cookieParser(cookieSecret));
 app.use(cookieEncrypter(cookieSecret));
@@ -62,7 +67,7 @@ app.use(cors());
 app.use(async (req, res, next) => {
   if (!ssb.client()) {
     setTimeout(() => {
-      console.log("Waiting for SSB to load...");
+      console.log('Waiting for SSB to load...');
 
       res.redirect(req.originalUrl);
     }, 500);
@@ -76,7 +81,7 @@ app.use(async (req, res, next) => {
 
   let key;
   try {
-    key = req.signedCookies["ssb_key"];
+    key = req.signedCookies.ssb_key;
     if (key) key = JSON.parse(key);
   } catch (_) {}
   if (!key || !key.id) return next();
@@ -86,41 +91,43 @@ app.use(async (req, res, next) => {
   next();
 });
 
-app.post("/login", async (req, res) => {
-    res.cookie("ssb_key", JSON.stringify(req.body.key), cookieOptions);
-    return res.json({status: "ok"});
+app.post('/login', async (req, res) => {
+  res.cookie('ssb_key', JSON.stringify(req.body.key), cookieOptions);
+  return res.json({ status: 'ok' });
 });
 
-app.use("/pub_invite", async (_req, res) => {
+app.use('/pub_invite', async (_req, res) => {
   const invite = await ssb.client().invite.create({ uses: 1 });
 
   res.json({ invite });
 });
-  
-app.post("/publish", async (req, res) => {
-    let key;
-    if (req.body.key == null) {
-        key = req.context.key;
-    } else {
-        key = req.body.key;
-    }
-    try {
-        await ssb.client().identities.publishAs({
-            key: key,
-            private: false,
-            content: req.body.content,
-        });
-    } catch(e) {
-        console.log("publish failed:", e);
-        return res.json({status: "fail"});
-    }
 
-    return res.json({status: "ok"});
+app.post('/publish', async (req, res) => {
+  let key;
+  if (req.body.key) {
+    key = req.body.key;
+  } else if (req.context.key) {
+    key = req.context.key;
+  } else {
+    throw httpErrorInfo(401, 'No ssb key provided on publish.');
+  }
+  try {
+    await ssb.client().identities.publishAs({
+      key,
+      private: false,
+      content: req.body.content,
+    });
+  } catch (e) {
+    console.log('publish failed:', e);
+    return res.json({ status: 'fail' });
+  }
+
+  return res.json({ status: 'ok' });
 });
 
-app.use("/transactions", async (_req, res) => {
-    try {
-        const query = `
+app.use('/transactions', async (_req, res) => {
+  try {
+    const query = `
         query { allPromises {
             id
             author {
@@ -160,18 +167,18 @@ app.use("/transactions", async (_req, res) => {
                 }
             }
         }}`;
-      
-        let r = await axios.post('http://127.0.0.1:4000', {query:query}, {});
-        return res.json(r.data.data.allPromises);
-    } catch(e) {
-        console.log(e);
-        return res.json({status: "fail"});
-    }
+
+    const r = await axios.post('http://127.0.0.1:4000', { query }, {});
+    return res.json(r.data.data.allPromises);
+  } catch (e) {
+    console.log(e);
+    return res.json({ status: 'fail' });
+  }
 });
 
-app.use("/identities", async (_req, res) => {
-    try {
-        const query = `query  { allIdMsgs {
+app.use('/identities', async (_req, res) => {
+  try {
+    const query = `query  { allIdMsgs {
             author {
                 id
             }
@@ -187,67 +194,81 @@ app.use("/identities", async (_req, res) => {
             }
           }
         }`;
-      
-        let r = await axios.post('http://127.0.0.1:4000', {query:query}, {});
-        return res.json(r.data.data.allIdMsgs);
-    } catch(e) {
-        console.log(e);
-        return res.json({status: "fail"});
-    }
+
+    const r = await axios.post('http://127.0.0.1:4000', { query }, {});
+    return res.json(r.data.data.allIdMsgs);
+  } catch (e) {
+    console.log(e);
+    return res.json({ status: 'fail' });
+  }
 });
 
-app.post("/uploadAdminKeyfile", (req, res) => {
-    // accessing the file
-    const myFile = req.files.file;
-    console.log('file retreived:', myFile.name, "saving as:", req.body.filename);
+app.post('/uploadAdminKeyfile', (req, res) => {
+  // accessing the file
+  const myFile = req.files.file;
+  console.log('file retreived:', myFile.name, 'saving as:', req.body.filename);
 
-    //  mv() method places the file inside public directory
-    myFile.mv(keyshareDir+req.body.filename, err => {
-        if (err) {
-            console.log(err)
-            return res.json({ msg: "Error occured" });
-        }
+  //  mv() method places the file inside public directory
+  myFile.mv(keyshareDir + req.body.filename, err => {
+    if (err) {
+      console.log(err);
+      return res.json({ msg: 'Error occured' });
+    }
 
-        return res.json({saved: req.body.filename, status: "ok"});
-    });
+    return res.json({ saved: req.body.filename, status: 'ok' });
+  });
 });
 
-app.post("/removeAdminKeyfile", (req, res) => {
-    fs.unlink(keyshareDir+req.body.filename, err => {
-        if (err) {
-            console.log(err);
-            return res.json({ msg: "Error occured" });
-        } else {
-            console.log('removing file', req.body.filename);
-        }
+app.post('/removeAdminKeyfile', (req, res) => {
+  fs.unlink(keyshareDir + req.body.filename, err => {
+    if (err) {
+      console.log(err);
+      return res.json({ msg: 'Error occured' });
+    }
+    console.log('removing file', req.body.filename);
 
-        return res.json({deleted: req.body.filename, status: "ok"});
-      });
+    return res.json({ deleted: req.body.filename, status: 'ok' });
+  });
 });
 
-app.post("/pollAdminKeyfiles", async (_req, res) => {
-    let nFound = 0;
-    var outObj = {'1': false, '2': false, '3': false, '4': false};
-    let cmd = keyshareDir+'checkAddressFromShards.sh';
-    for (let i=0; i<4; i++) {
-        let path = `${keyshareDir}${i+1}.json`;
-        if (fs.existsSync(path)) {
-            outObj[(i+1).toString()] = true;
-            nFound += 1;
-            cmd += ' '+path;
-        }
+app.post('/pollAdminKeyfiles', async (_req, res) => {
+  let nFound = 0;
+  const outObj = { '1': false, '2': false, '3': false, '4': false };
+  let cmd = `${keyshareDir}checkAddressFromShards.sh`;
+  for (let i = 0; i < 4; i++) {
+    const path = `${keyshareDir}${i + 1}.json`;
+    if (fs.existsSync(path)) {
+      outObj[(i + 1).toString()] = true;
+      nFound += 1;
+      cmd += ` ${path}`;
     }
-    if (nFound > 2) {
-        try {
-            let { stdout } = await sh(cmd);
-            outObj.address = stdout.replace(/(\r\n|\n|\r)/gm,"");
-        } catch(_e) {
-            outObj.address = "n/a";
-        }
-    } else {
-        outObj.address = "n/a";
+  }
+  if (nFound > 2) {
+    try {
+      const { stdout } = await sh(cmd);
+      outObj.address = stdout.replace(/(\r\n|\n|\r)/gm, '');
+    } catch (_e) {
+      outObj.address = 'n/a';
     }
-    res.json(outObj);
+  } else {
+    outObj.address = 'n/a';
+  }
+  res.json(outObj);
+});
+
+// handle errors of type created by the 'httpErrorInfo' method
+app.use((error, req, res, next) => {
+  // Sets HTTP status code
+  res.status(error.status);
+
+  console.log(`${error.status} - ${error.message}`);
+  console.log(error.stack);
+
+  // Sends response
+  res.json({
+    status: error.status,
+    message: error.message,
+  });
 });
 
 // In production we need to pass these values in instead of relying on webpack
@@ -268,27 +289,27 @@ app.get('*.js', (req, res, next) => {
   next();
 });
 
-if (sentry) {
-  // The error handler must be before any other error middleware and after all controllers
-  app.use(sentry.Handlers.errorHandler());
-}
-
 app.use((error, _req, res, _next) => {
   res.statusCode = 500;
-  res.render("desktop/error", { error });
+  res.render('desktop/error', { error });
 });
 
-var expressServer;
+let expressServer;
 if (process.env.HTTPS) {
-    console.log("PROTOCOL: HTTPS (secure)");
-    expressServer = https.createServer({
-        key: fs.readFileSync(process.env.PROD_KEYFILE||'serverTest.key'),
-        cert: fs.readFileSync(process.env.PROD_CERT||'serverTest.cert')
-      }, app).listen(port, host, async err => {
+  console.log('PROTOCOL: HTTPS (secure)');
+  expressServer = https
+    .createServer(
+      {
+        key: fs.readFileSync(process.env.PROD_KEYFILE || 'serverTest.key'),
+        cert: fs.readFileSync(process.env.PROD_CERT || 'serverTest.cert'),
+      },
+      app,
+    )
+    .listen(port, host, async err => {
       if (err) {
         return logger.error(err.message);
       }
-    
+
       // Connect to ngrok in dev mode
       if (ngrok) {
         let url;
@@ -303,25 +324,25 @@ if (process.env.HTTPS) {
       }
     });
 } else {
-    console.log("PROTOCOL: HTTP (insecure)");
-    expressServer = app.listen(port, host, async err => {
-        if (err) {
-          return logger.error(err.message);
-        }
-      
-        // Connect to ngrok in dev mode
-        if (ngrok) {
-          let url;
-          try {
-            url = await ngrok.connect(port);
-          } catch (e) {
-            return logger.error(e);
-          }
-          logger.appStarted(port, prettyHost, url);
-        } else {
-          logger.appStarted(port, prettyHost);
-        }
-    });
+  console.log('PROTOCOL: HTTP (insecure)');
+  expressServer = app.listen(port, host, async err => {
+    if (err) {
+      return logger.error(err.message);
+    }
+
+    // Connect to ngrok in dev mode
+    if (ngrok) {
+      let url;
+      try {
+        url = await ngrok.connect(port);
+      } catch (e) {
+        return logger.error(e);
+      }
+      logger.appStarted(port, prettyHost, url);
+    } else {
+      logger.appStarted(port, prettyHost);
+    }
+  });
 }
 
 module.exports = expressServer;
--- server/lib/errors.js
+++ server/lib/errors.js
@@ -1,9 +1,11 @@
-const Sentry = require("@sentry/node");
+const Sentry = require('@sentry/node');
 
 let sentry;
 
-if (process.env.NODE_ENV == "production") {
-  const SENTRY_DSN = process.env.SENTRY_DSN;
+if (process.env.NODE_ENV === 'production') {
+  const SENTRY_DSN =
+    process.env.SENTRY_DSN ||
+    'https://e68dbc1c822647c28af109d194d2882f@o453511.ingest.sentry.io/5442376cashless-lr';
   if (SENTRY_DSN) {
     Sentry.init({
       dsn: SENTRY_DSN,
@@ -11,24 +13,24 @@ if (process.env.NODE_ENV == "production") {
     sentry = Sentry;
   }
 
-  process.on("unhandledRejection", (reason, _promise) => {
-    console.error("unhandledRejection", reason);
+  process.on('unhandledRejection', (reason, _promise) => {
+    console.error('unhandledRejection', reason);
     if (sentry) {
       Sentry.captureException(new Error(reason.toString()));
     }
     setTimeout(() => {
-      console.log("Harakiri because of unhandledRejection");
+      console.log('Harakiri because of unhandledRejection');
       process.exit(1);
     }, 1000);
   });
 
-  process.on("uncaughtException", (err) => {
-    console.error("uncaughtException", err.message);
+  process.on('uncaughtException', err => {
+    console.error('uncaughtException', err.message);
     if (sentry) {
       Sentry.captureException(err);
     }
     setTimeout(() => {
-      console.log("Harakiri because of uncaughtException");
+      console.log('Harakiri because of uncaughtException');
       process.exit(1);
     }, 1000);
   });
